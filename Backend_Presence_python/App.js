e3ee253c74183d6d9ed9268504aa1b721915b19f
MagloireNgassa <62631084+MagloireNgassa@users.noreply.github.com>
2021-09-26 21:17:41 -0400

    Update App.js

--- Backend_Presence_python/App.js
+++ Backend_Presence_python/App.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //mongoose.connect('mongodb://localhost:27017/PresenceBd',{ useUnifiedTopology: true, useNewUrlParser: true });
 
-mongoose.connect('mongodb+srv://SaoGroupe:barnou052018@clustersaogroupe.n7o0s.mongodb.net/PresenceDB?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://SaoGroupe:mot_de_passe@clustersaogroupe.n7o0s.mongodb.net/PresenceDB?retryWrites=true&w=majority');
 
 //mongoose.connect('mongodb://mngassa:mngassa@10.30.40.121:27017/mngassa',{​​​​​​​ useUnifiedTopology: true, useNewUrlParser: true }​​​​​​​  );
 
